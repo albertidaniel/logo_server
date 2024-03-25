b5049c58375da04e484545eaf2f5bda7f282bee6
albertidaniel <albertidaniel962@gmail.com>
2024-03-25 11:14:48 +0100

    adding new port

--- server.js
+++ server.js
@@ -170,7 +170,9 @@ app.put('/update', (req, res) => {
         });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app its running on port ${process.env.PORT}`);
+});
 
 /*
 / --> this is working
